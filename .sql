0574fde77b438e039d70364d300ac0794e18a4da
Lucas Amaral <lukinhasilvaamaral@gmail.com>
2022-03-25 20:36:01 -0300

    first commit

---
+++ .sql
@@ -0,0 +1,23 @@
+CREATE TABLE user (
+  id serial primary key,
+  name varchar(100) not null,
+  email varchar(150) not null,
+  password varchar(50) not null
+  );
+
+
+CREATE TABLE movie(
+  id serial primary key,
+  title varchar(100) not null,
+  description varchar(1000) not null,
+  user_id integer not null,
+  foreign key (user_id) references user (id),
+  genderId integer not null,
+  foreign key (genderId) references gender (id),
+);
+
+CREATE TABLE gender(
+  id serial primary key,
+  name VARCHAR(100),
+)
+;
